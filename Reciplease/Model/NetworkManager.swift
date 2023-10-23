e51f06ac191b8cc8fde9a471cebf1684296a33dd
Melvin Poutrel <melvin.poutrel@itinnove.com>
2023-10-23 22:43:14 +0200

    Add Network Manager for API calls

--- Reciplease/Model/NetworkManager.swift
+++ Reciplease/Model/NetworkManager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkManager: NetworkProtocol {
+    func request(url: URL, completionHandler: @escaping (DataResponse<RecipesHits, AFError>) -> Void) {
+        let request = AF.request(url) { $0.timeoutInterval = 10 }.validate()
+        request.responseDecodable(completionHandler: completionHandler)
+    }
+}
+
+protocol NetworkProtocol {
+    func request(url: URL, completionHandler: @escaping(DataResponse<RecipesHits, AFError>)->Void)
+}
